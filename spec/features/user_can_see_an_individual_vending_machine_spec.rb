276a43230fcd4324cf4531622863c164abfbd402
Nancy Lee <srlee713@gmail.com>
2019-09-06 09:23:23 -0600

    Complete user story 1

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -1,12 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'When a user visits a vending machine show page', type: :feature do
-  scenario 'they see the location of that machine' do
-    owner = Owner.create(name: "Sam's Snacks")
-    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+  before :each do
+    @owner = Owner.create(name: "Sam's Snacks")
+    @dons  = @owner.machines.create(location: "Don's Mixed Drinks")
 
-    visit machine_path(dons)
+    @snack_1 = @dons.snacks.create(name: "White Castle Burger", price: 3.50)
+    @snack_2 = @dons.snacks.create(name: "Pop Rocks", price: 1.50)
+    @snack_3 = @dons.snacks.create(name: "Flaming Hot Cheetos", price: 2.50)
+  end
+
+  scenario 'they see the location of that machine' do
+    visit machine_path(@dons)
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
+  end
+
+  scenario 'they see the name of all of the snacks associated with that vending machine along with their price' do
+    visit machine_path(@dons)
+
+    within "#snack-id-#{@snack_1.id}" do
+      expect(page).to have_content(@snack_1.name)
+      expect(page).to have_content(@snack_1.price)
+    end
+
+    within "#snack-id-#{@snack_2.id}" do
+      expect(page).to have_content(@snack_2.name)
+      expect(page).to have_content(@snack_2.price)
+    end
+
+    within "#snack-id-#{@snack_3.id}" do
+      expect(page).to have_content(@snack_3.name)
+      expect(page).to have_content(@snack_3.price)
+    end
   end
 end
