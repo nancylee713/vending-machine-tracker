195f653d5ba9a0f359acd2074de3030bb3d945d7
Nancy Lee <srlee713@gmail.com>
2019-09-06 09:23:23 -0600

    Add a model test for count_snacks method in machine model
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>
                    Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>
                    Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe Machine, type: :model do
     it { should have_many(:snack_machines) }
     it { should have_many(:snacks).through(:snack_machines) }
   end
+
+  describe 'instance methods' do
+    it "counts number of distinct snack items" do
+      owner = Owner.create!(name: "Jen")
+      machine_1 = owner.machines.create!(location: "Don's Mixed Drinks")
+      snack_1 = machine_1.snacks.create!(name: "White Castle Burger", price: 3.50)
+      snack_2 = machine_1.snacks.create!(name: "White Castle Burger", price: 3.50)
+      snack_3 = machine_1.snacks.create!(name: "Pop Rocks", price: 1.50)
+      snack_4 = machine_1.snacks.create!(name: "Flaming Hot Cheetos", price: 2.50)
+
+      expect(machine_1.count_snacks).to eq(3)
+    end
+  end
 end
