4a0a914d8b5bdcce2bb29fc95d2909dcf56332df
Nancy Lee <srlee713@gmail.com>
2019-09-06 09:23:23 -0600

    Add numericality validation test for snack model
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>
                    Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>
                    Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Snack, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :price }
+    it { should validate_numericality_of :price }
   end
 
   describe 'relationships' do
