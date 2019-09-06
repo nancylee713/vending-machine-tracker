fe5512b4e57a2ee7a563ba5948ade83b8f29cc34
Nancy Lee <srlee713@gmail.com>
2019-09-06 09:23:23 -0600

    Update seed file
    Co-authored-by: Tyler Schaffer <48742436+tschaffer1618@users.noreply.github.com>
                    Leiya Kenney <45922590+leiyakenney@users.noreply.github.com>
                    Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+SnackMachine.destroy_all
+Snack.destroy_all
+Machine.destroy_all
+Owner.destroy_all
+
+owner = Owner.create(name: "Jen")
+machine_1 = Machine.create(location: "Don's Mixed Drinks")
+machine_2 = Machine.create(location: "Turing Basement")
+
+owner.machines << machine_1
+owner.machines << machine_2
+
+snack_1 = Snack.create(name: "White Castle Burger", price: 3.50)
+snack_2 = Snack.create(name: "Pop Rocks", price: 1.50)
+snack_3 = Snack.create(name: "Flaming Hot Cheetos", price: 2.50)
+
+machine_1.snacks << snack_1
+machine_1.snacks << snack_2
+machine_2.snacks << snack_1
+machine_2.snacks << snack_3
